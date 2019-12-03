d0c68f533de0e66c7a7fd6a072a20c582f4d2ff4
DAPL-Asset-201 <tonmoy.malik@gmail.com>
2019-12-03 12:39:08 +0530

    listen port changed from 5000 to 8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 const session = require('express-session');
 const flash = require('express-flash')
 const Mongo = require("mongodb");
@@ -100,5 +100,5 @@ express()
     });
   })
   .listen(PORT, () => {
-    console.log('Listening  to  port ' + PORT)
+    console.log('Express server listening on port ', PORT)
   })
